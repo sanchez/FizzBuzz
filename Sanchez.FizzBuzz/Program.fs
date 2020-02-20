d6a8f5a5766b1dd8f7a091e9d3c5fd4bf26046d0
sanchez <dfitz360@gmail.com>
2020-02-20 20:51:17 +1000

    Created fizzbuzz

---
+++ Sanchez.FizzBuzz/Program.fs
@@ -0,0 +1,25 @@
+﻿[<AutoOpen>]
+module FizzBuzz =
+    let (|Fizz|_|) arg =
+        if (arg % 3) = 0 then Some ()
+        else None
+        
+    let (|Buzz|_|) arg =
+        if (arg % 5) = 0 then Some ()
+        else None
+
+[<EntryPoint>]
+let main argv =
+    let items =
+        Seq.initInfinite id
+        |> Seq.skip 1
+    
+    items
+    |> Seq.map (function
+        | Fizz & Buzz -> "FizzBuzz"
+        | Fizz -> "Fizz"
+        | Buzz -> "Buzz"
+        | rem -> sprintf "%d" rem)
+    |> Seq.iter (printf "%s, ")
+    
+    0
